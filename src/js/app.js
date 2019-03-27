2fa7e4faffc84e06416ecf2343f3c5bedbcf2711
vfilinsly <vfilinsky.sells@gmail.com>
2019-03-27 22:53:10 +0500

    adaptive

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,27 @@ import owlCarousel from 'owl.carousel';
 import magnificPopup from 'magnific-popup';
 
 $(document).ready(function() {
+// Изменения для мобильной версии
+  $(window).on('load resize', function() {
+    if ($(window).width() <= 600) {
+      // Отключаем owlCarousel
+      $('.listimg__slider.owl-carousel').removeClass('js-sl-method owl-carousel');
+      // Переносим nav за footer,добавляем классы
+      $('nav').insertAfter('footer');
+      $('nav').find('ul > li > a').removeClass();
+      $('nav').addClass('menu');
 
+
+    }
+  });
+  // MENU OPEN
+  $('.mobile__nav').click(function() {
+    $('.menu').toggleClass('menu-open');
+  });
+  $('.close').click(function() {
+    $('.menu').removeClass('menu-open');
+  });
+  // MODALS
   $('a[href="#text"], a[href="#callback"], a[href="#thanks"], a[href="#slider"]').magnificPopup({
     type: 'inline',
     removalDelay: 500,
@@ -13,27 +33,12 @@ $(document).ready(function() {
         this.st.mainClass = this.st.el.attr('data-effect');
       },
       open: function() {
-        //$('.modal').addClass('modal-logo').delay(300).fadeIn();
       }
     },
     midClick: true
   });
-  	var hash = location.hash.slice(1);
-	 if( hash === 'm_1') {
-	 	$('a[href="#m_1"]').addClass('list__active');
-	 }
-	 else if(hash === 'm_2') {
-	 	$('a[href="#m_2"]').addClass('list__active');
-	 }
-	 else {
-	 	$('a[href="#m_1"]').addClass('list__active');
-	 }
 
-  $('.listimg__left').on('click', 'a', function() {
-  	$('.listimg__left .list__active').removeClass();
-  	$(this).addClass('list__active');
-  });
-
+  // MODAL "GALERY"
   $('#slider').owlCarousel({
     items:1,
     loop:true,
@@ -58,7 +63,7 @@ $(document).ready(function() {
         youtube: {
           index: 'youtube.com/',
           id: 'v=', 
-          src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
+          src: '//www.youtube.com/embed/%id%?autoplay=1'
         },
         vimeo: {
           index: 'vimeo.com/',
@@ -70,19 +75,36 @@ $(document).ready(function() {
           src: '%id%&output=embed'
         }
       },
-      srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+      srcAction: 'iframe_src',
     },
     callbacks: {
       beforeOpen: function() {
         this.st.mainClass = this.st.el.attr('data-effect');
       },
       open: function() {
-        //$('.modal').addClass('modal-logo').delay(300).fadeIn();
       }
     }
   });
 
-  $('.listimg__slider').owlCarousel({
+  // Активация элементов в блоке Method
+  var hash = location.hash.slice(1);
+  if( hash === 'm_1') {
+    $('a[href="#m_1"]').addClass('list__active');
+  }
+  else if(hash === 'm_2') {
+    $('a[href="#m_2"]').addClass('list__active');
+  }
+  else {
+    $('a[href="#m_1"]').addClass('list__active');
+  }
+  // при клике
+  $('.listimg__left-box').on('click', function() {
+    $('.listimg__left-box.list__active').removeClass('list__active');
+    $(this).addClass('list__active');
+  });
+
+  // METHOD SLIDER
+  $('.js-sl-method').owlCarousel({
   	items:1,
     loop:false,
     mouseDrag: false,
@@ -92,7 +114,7 @@ $(document).ready(function() {
     autoplayHoverPause:true,
     startPosition: 'URLHash'
   });
-
+  // STAGE SLIDER
   $('.stage-slider').owlCarousel({
   	items:1,
     loop:false,
@@ -104,12 +126,12 @@ $(document).ready(function() {
       				'<span class="stage__nav-next stage__nav">&rarr;</span>'],
     nav:true,
     URLhashListener:true,
-    autoplayHoverPause:true,
+    autoplayHoverPause:false,
     startPosition: 'URLHash'
   });
-
+  // добавляем блок span рядом с кнопкой слайдера
   $('span.stage__nav-next').parent().after('<span class="stage__nav-text">i этап</span>');
-
+  // отслеживаем хеш и делаем нужную ссылку активной
 	 	 if( hash === 'floor-1') {
 	 	$('span.stage__nav-text').text('i этап');
 	 	$('.stage__floor').find('li > a').removeClass();
@@ -130,7 +152,7 @@ $(document).ready(function() {
 	 	$('.stage__floor').find('li > a').removeClass();
 	 	$("a[href='#" + hash + "']").addClass('stage__floor-active');
   }
-
+  // изменяем текст при переключении слайдера,отслеживая хеш 
   $('.stage__floor > li, .stage__nav-prev, .stage__nav-next').on('click', function() {
   	setTimeout (function() {
   		var hash = location.hash.slice(1);
@@ -138,7 +160,6 @@ $(document).ready(function() {
       $('.stage__floor').find('li > a').removeClass();
       link.addClass('stage__floor-active');
   		if( hash === 'floor-1') {
-
 	 	$('span.stage__nav-text').text('i этап');
 	 }
 	 else if(hash === 'floor-2') {
@@ -151,8 +172,6 @@ $(document).ready(function() {
 	 	$('span.stage__nav-text').text('iv этап');
       }
   	},200);		
-  	
-  	
-    
   });
+
 });
